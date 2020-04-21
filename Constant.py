bd10379db0ba6144399cb2c764ff60f5230c66f0
yuanchingchen <yuanching.chen.appledev@gmail.com>
2020-04-21 15:29:26 -0500

    Added Constants file

---
+++ Constant.py
@@ -0,0 +1,5 @@
+
+
+DATE_FORMAT = "%m-%d-%Y"
+DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
+DATA_URL_POSTFIX = ".csv"
